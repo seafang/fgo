7e4e0a96318c9ee79cd59958da21c78b2a3d3691
seafang <47890687+seafang@users.noreply.github.com>
2019-02-28 23:14:14 +0000

    Update and rename jquery.js to misc.js

--- jquery.js
+++
@@ -1,6 +0,0 @@
-//menu button
-$(document).ready(function() {
-	$("a.menu").click(function() {
-		$("#menu-content").toggle(300);
-	});
-});
---
+++ misc.js
@@ -0,0 +1,20 @@
+//menu button
+$(document).ready(function() {
+	$("a.menu").click(function() {
+		$("#menu-content").toggle(300);
+	});
+});
+
+// To top button
+$(document).scroll(function(){
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    $("#to_top").show();
+  } else {
+    $("#to_top").hide();
+  }
+}
+
+function toTop() {
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0;
+} 
