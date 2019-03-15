74a879b93d722abfeff4911a2bc43a4dc80660bb
seafang <47890687+seafang@users.noreply.github.com>
2019-03-15 02:31:49 +0000

    Update data.js

--- js/data.js
+++ js/data.js
@@ -7,10 +7,21 @@ var servants = JSON.parse(servant);
  * @param  {Object} filters: an object with the filter criteria as the property names
  * @return {Array}
  */
-function multiFilter(array, filters) {
+
+/*function multiFilter(array, filters) {
 	const filterKeys = Object.keys(filters);
 	return array.filter((item) => {
 		return filterKeys.every(key => {
+			if (!filters[key].length) return true;
+			return filters[key].includes(item[key]);
+		});
+	});
+}*/
+
+function multiFilter(array, filters) {
+	var filterKeys = Object.keys(filters);
+	return array.filter(function (item) {
+		return filterKeys.every(function (key) {
 			if (!filters[key].length) return true;
 			return filters[key].includes(item[key]);
 		});
