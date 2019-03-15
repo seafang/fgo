7585d2c6898569d7b211de5c83a1c19be073e65f
seafang <47890687+seafang@users.noreply.github.com>
2019-03-15 04:48:14 +0000

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -11,28 +11,28 @@ function pickEnemy(type, enemyID) {
 			return obj.id == enemyID;
 		});
 	}
-	$("#current-enemy-name").html(enemyInfo.name);
+	$("#current-enemy-name").html(enemyInfo[0].name);
 	if (enemyInfo.classes) {
-		$("#current-enemy-class").val(enemyInfo.classes);
+		$("#current-enemy-class").val(enemyInfo[0].classes);
 	} else {
 		$("#current-enemy-class").val("Saber");
 	}
-	$("#current-enemy-gender").val(enemyInfo.gender);
-	$("#current-enemy-attribute").val(enemyInfo.attribute);
-	$("#current-enemy-alignment1").val(enemyInfo.alignment1);
-	$("#current-enemy-alignment2").val(enemyInfo.alignment2);
-	$("#current-enemy-trait").html(enemyInfo.trait);
+	$("#current-enemy-gender").val(enemyInfo[0].gender);
+	$("#current-enemy-attribute").val(enemyInfo[0].attribute);
+	$("#current-enemy-alignment1").val(enemyInfo[0].alignment1);
+	$("#current-enemy-alignment2").val(enemyInfo[0].alignment2);
+	$("#current-enemy-trait").html(enemyInfo[0].trait);
 }
 
 function setEnemy(element) {
 	$(element).show();
-	$(element).find(".enemy-name").html(enemyInfo.name);
-	$(element).find(".enemy-class").html(enemyInfo.classes);
-	$(element).find(".enemy-gender").html(enemyInfo.gender);
-	$(element).find(".enemy-attribute").html(enemyInfo.attribute);
-	$(element).find(".enemy-alignment1").html(enemyInfo.alignment1);
-	$(element).find(".enemy-alignment2").html(enemyInfo.alignment2);
-	$(element).find(".enemy-trait").html(enemyInfo.trait);
+	$(element).find(".enemy-name").html(enemyInfo[0].name);
+	$(element).find(".enemy-class").html(enemyInfo[0].classes);
+	$(element).find(".enemy-gender").html(enemyInfo[0].gender);
+	$(element).find(".enemy-attribute").html(enemyInfo[0].attribute);
+	$(element).find(".enemy-alignment1").html(enemyInfo[0].alignment1);
+	$(element).find(".enemy-alignment2").html(enemyInfo[0].alignment2);
+	$(element).find(".enemy-trait").html(enemyInfo[0].trait);
 }
 
 function resetEnemy(element) {
