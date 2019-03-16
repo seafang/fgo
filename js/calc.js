b5e65457ca2cabd9832cb8770bee728a77f2140c
seafang <47890687+seafang@users.noreply.github.com>
2019-03-16 07:03:42 +0000

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -44,7 +44,7 @@ function setEnemy(element) {
 	} else {
 		$(element).find(".enemy-img").attr("src", "images/bg_logo.png");
 	}
-	if ($("#current-enemy-name").html() == ""){
+	if ($("#current-enemy-name").html() == "未選定/自訂敵人"){
 		$(element).find(".enemy-name").html("自訂敵人");
 	} else {
 		$(element).find(".enemy-name").html($("#current-enemy-name").html());
