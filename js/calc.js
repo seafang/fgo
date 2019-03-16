b1d1cb0d1a8ed322360ae407863b1a27332c8b66
seafang <47890687+seafang@users.noreply.github.com>
2019-03-16 04:17:51 +0000

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -49,7 +49,7 @@ function setEnemy(element) {
 	} else {
 		$(element).find(".enemy-name").html($("#current-enemy-name").html());
 	}
-	var imgURL = "images/class/" + encodeURI($("#current-enemy-class").val()) + ".png";
+	var imgURL = "images/class/" + encodeURI($("#current-enemy-class").val()) + ".webp";
 	$(element).find(".enemy-class").attr({
 		src: imgURL,
 		alt: $("#current-enemy-class").val()
