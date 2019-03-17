d41f3b327948672f2e66119d8dbef826ed477506
seafang <47890687+seafang@users.noreply.github.com>
2019-03-17 18:46:08 +0000

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -8,49 +8,16 @@ $(document).on("scroll", function() {
 	iframeResize();
 });
 
-/*$("#activeFrame").contents().on("scroll", function() {
-	iframeResize();
-});*/
-
 function iframeResize() {
-	var height = $("#activeFrame").contents().height() + 300;
-	/*let frame = document.getElementById("activeFrame");
-	frame.style.height = (height + 200) + "px";*/
-	$("#activeFrame").css("height", height);
+	var frameHeight = $("#activeFrame").contents().find(".counter").html();
+	$("#activeFrame").css("height", frameHeight);
 }
-
-/* window.addEventListener('DOMContentLoaded', function() {
-	iframeResize();
-	initiateResize();
-});*/
 
 function openFrame(url, tab) {
 	$("nav a").removeClass("active");
 	$(tab).addClass("active");
 	$("#activeFrame").attr("src", url);
 }
-
-/*function iframeResize() {
-	let height = $("#activeFrame").contents().find($("body")).scrollHeight;
-	$("#activeFrame").css("height", (height + 200) + "px");
-}
-
-function initiateResize() {
-	let height = $("body").scrollHeight;
-	$("window").parentsUntil("body").find($("#activeFrame")).css("height", (height + 200) + "px");
-}*/
-
-/* function iframeResize() {
-	let height = document.getElementById("activeFrame").contentWindow.document.getElementsByTagName("body").scrollHeight;
-        let frame = document.getElementsByTagName("iframe")
-	frame.style.height = (height + 300) + "px";
-}
-
-function initiateResize() {
-	let height = $("body").scrollHeight;
-	let frame = window.parent.document.getElementById("activeFrame");
-	frame.style.height = (height + 300) + "px";
-}*/
 	
 // Menu button
 $(document).ready(function() {
