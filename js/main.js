b43a9dc562f239ddef1064b175e7075726a8aacd
seafang <47890687+seafang@users.noreply.github.com>
2019-04-01 03:07:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Load data
 var servants = JSON.parse(servant);
+var ce = JSON.parse(craftEssence);
 
 // Load save
 var currentSave = {};
-var bgServant = [], bgCE = [];
+var bgServant = [], bgCE = [], bgMaster = [];
