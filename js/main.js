a69ede61b1092782fec13782f9baf08020481d14
seafang <47890687+seafang@users.noreply.github.com>
2019-04-05 20:49:57 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ var servants = JSON.parse(servant);
 var ce = JSON.parse(craftEssence);
 var master = JSON.parse(mysticCode);
 
+var skillBuff = JSON.parse(servantSkillBuff);
+var npBuff = JSON.parse(servantNPBuff);
+var atk = JSON.parse(servantATK);
+var ceBuff =  JSON.parse(craftEssenceBuff);
+var masterBuff = JSON.parse(mysticCodeBuff);
+
 // Load save
 var currentSave = {};
 var bgServant = [], bgCE = [], bgMaster = [];
