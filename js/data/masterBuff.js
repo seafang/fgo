e7cf28e195984e70beb3e5055b4f381310e62e7b
seafang <47890687+seafang@users.noreply.github.com>
2019-04-14 02:51:24 +0100

    Update masterBuff.js

--- js/data/masterBuff.js
+++ js/data/masterBuff.js
@@ -1,1 +1,1 @@
-mysticCodeBuff = '[{"1":30,"2":32,"3":34,"4":36,"5":38,"6":40,"7":42,"8":44,"9":46,"10":50,"masterName":"迦勒底","no":"skill2","range":"single","effect":"dmg","lookUp":"","corrDetail":"","rateFixed":false}]';
+mysticCodeBuff = '[{"1":30,"2":32,"3":34,"4":36,"5":38,"6":40,"7":42,"8":44,"9":46,"10":50,"name":"迦勒底","no":"skill2","range":"single","effect":"dmg","lookUp":"","corrDetail":""}]';
