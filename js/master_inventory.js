482f95c5574407e8ce5c1a1a525ecac53f0d5cb6
seafang <47890687+seafang@users.noreply.github.com>
2019-04-17 23:55:26 +0100

    Update master_inventory.js

--- js/master_inventory.js
+++ js/master_inventory.js
@@ -3,16 +3,29 @@ var currentSave = parent.currentSave;
 var bgMaster = parent.bgMaster;
 
 $(document).ready(function() {
-	generateMasterInventory();
-	loadMasterSave();
+	updateCounter();
+	generateMasterInventory();		// Generate inventory table
+	loadMasterSave();		// Load saved data
 });
 
+// Update page height
+$(document).on("click", function() {
+	updateCounter();
+});
+
+function updateCounter() {
+	var height = $("#master-inventory-content").outerHeight() + 500;
+	$("#master-inventory-counter").html(height);
+}
+
+// Clear current table
 function clearMasterInventory() {
 	$("#master-inventory").find(".master-inventory-row").each(function(){
 		$(this).remove();
 	});
 }
 
+// Generate inventory table
 function generateMasterInventory() {
 	clearMasterInventory();
 	var table = document.getElementById("master-inventory");
@@ -36,9 +49,9 @@ function generateMasterInventory() {
 	});
 }
 
-// Apply saved data
+// Load saved data
 function loadMasterSave() {
-	if (bgMaster[0] !== undefined) {
+	if (bgMaster[0] !== undefined) {		// Check if save exists
 		$("#master-inventory").find(".master-inventory-row").each(function(){
 			var rowName = $(this).find("td:first").html();
 			var ownershipToggle = $(this).find(".master-owned");
@@ -46,32 +59,38 @@ function loadMasterSave() {
 				return obj.name == rowName;
 			});
 			if (master[0] !== undefined) {
-				if (master[0].data[0] == true) {
+				if (master[0].data[0] == true) {		// Toggle if the mystic code is owned
 					$(this).find(".master-owned").attr("checked", true);
 					enableMasterOption(ownershipToggle);
 				}
-				$(this).find(".master-inventory-lv").val(master[0].data[1]);
+				$(this).find(".master-inventory-lv").val(master[0].data[1]);	// Update mystic code lv
 			}
 		});
 	}
 }
 
-// Update data
 $(document).ready(function() {
-	$("#inventory-row-迦勒底").find(".master-owned").prop("disabled", true);
+	$("#inventory-row-迦勒底").find(".master-owned").prop("disabled", true);		// 迦勒底 is owned by default
+	
+	// Update save on change
 	$("select").change(function() {
 		updateMaster(this);
 	});
+	
+	// Enable toggles on change in ownership
 	$(".master-owned").change(function() {
 		updateMasterOwnership(this);
 		enableMasterOption(this);
 	});
 });
 
+// Update mystic code save data
 function updateMaster(element) {
 	var row = $(element).parents("tr");
 	var rowName = $(row).find("td:first").html();
 	var info = {};
+	
+	// If there is an existing save for the mystic code, delete old save
 	if (bgMaster[0] !== undefined) {
 		var position = bgMaster.findIndex(function(obj) {
 			return obj.name == rowName; 
@@ -80,10 +99,13 @@ function updateMaster(element) {
 			bgMaster.splice(position, 1);
 		}
 	}
+	
 	info.name = rowName;
 	info.data = [];
-	info.data[0] = $(row).find(".master-owned").is(":checked");
-	info.data[1] = Number($(row).find(".master-inventory-lv").val());
+	info.data[0] = $(row).find(".master-owned").is(":checked");		// Ownership
+	info.data[1] = Number($(row).find(".master-inventory-lv").val());	// Mystic code lv
+	
+	// Sync with background data
 	bgMaster.push(info);
 	currentSave.master = bgMaster;
 	parent.bgMaster = bgMaster;
@@ -91,6 +113,7 @@ function updateMaster(element) {
 	save();
 }
 
+// Update ownership status
 function updateMasterOwnership(element) {
 	var row = $(element).parents("tr");
 	var rowName = $(row).find("td:first").html();
@@ -98,24 +121,25 @@ function updateMasterOwnership(element) {
 	var position = master.findIndex(function(obj) {
 		return obj.name == rowName;
 	});
-	master[position].owned = newValue;
+	master[position].owned = newValue;		// Update ownership status in database, allows filtering when needed
 	parent.master = master;
 }
 
+// Enable toggles on change in ownership
 function enableMasterOption(element) {
 	var row = $(element).parents("tr");
 	var rowName = $(row).find("td:first").html();
-	if ($(element).is(":checked")) {
+	if ($(element).is(":checked")) {		// When ownership is toggled 'true'
 		$(row).find(".master-inventory-lv").prop("disabled", false);
 		$(row).find(".skill1-logo").removeClass("dull");
 		$(row).find(".skill2-logo").removeClass("dull");
 		$(row).find(".skill3-logo").removeClass("dull");
-	} else {
+	} else {					// Initialise all fields on disable
 		$(row).find(".master-inventory-lv").val(0);
 		$(row).find(".master-inventory-lv").prop("disabled", true);
 		$(row).find(".skill1-logo").addClass("dull");
 		$(row).find(".skill2-logo").addClass("dull");
 		$(row).find(".skill3-logo").addClass("dull");
-		update(element);
+		updateMaster(element);
 	}
 }
