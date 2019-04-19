7a9500b563e4b0ef4ce31ada09ba7081e7c4c96c
seafang <47890687+seafang@users.noreply.github.com>
2019-04-19 05:30:19 +0100

    Update inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -178,8 +178,6 @@ function generateInventory() {
 			"<option value='3'>3</option><option value='4'>4</option><option value='5'>5</option><option value='6'>6</option><option value='7'>7</option>" + 			
 			"<option value='8'>8</option><option value='9'>9</option><option value='10'>10</option></select>";			
 		row.insertCell(-1).innerHTML = "<label class='switch'><input type='checkbox' class='skill3-rankup' value='true' disabled><span class='slider'></span></label>";			
-		row.insertCell(-1).innerHTML = "";				
-		row.insertCell(-1).innerHTML = "";	
 		row.insertCell(-1).innerHTML = "";
 		row.insertCell(-1).innerHTML = "<input type='number' class='narrow event-ED' value='0' min='0' disabled>";			
 	});
